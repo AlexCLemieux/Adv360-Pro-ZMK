eb977043ab35cc721235d4e5721fc709c7c7bac7
AlexLemieux <alexclemieux@gmail.com>
2026-04-06 22:28:47 -0400

    trying to make updates to include urobs zmk helpers

---
+++ config/keys_adv360.h
@@ -0,0 +1,102 @@
+// Key positions for Kinesis Advantage 360 Pro (76 keys)
+// Row 0: function/number row
+#define L00  0   // =
+#define L01  1   // 1
+#define L02  2   // 2
+#define L03  3   // 3
+#define L04  4   // 4
+#define L05  5   // 5
+#define L06  6   // mod1 (tog layer)
+
+#define R00  7   // mod2
+#define R01  8   // 6
+#define R02  9   // 7
+#define R03  10  // 8
+#define R04  11  // 9
+#define R05  12  // 0
+#define R06  13  // -
+
+// Row 1: QWERTY top
+#define L10  14  // Tab
+#define L11  15  // Q
+#define L12  16  // W
+#define L13  17  // E
+#define L14  18  // R
+#define L15  19  // T
+#define L16  20  // mod3
+
+#define R10  21  // mod4
+#define R11  22  // Y
+#define R12  23  // U
+#define R13  24  // I
+#define R14  25  // O
+#define R15  26  // P
+#define R16  27  // backslash
+
+// Row 2: home row
+#define L20  28  // Caps
+#define L21  29  // A
+#define L22  30  // S
+#define L23  31  // D
+#define L24  32  // F
+#define L25  33  // G
+#define L26  34  // mod5
+
+#define R20  39  // mod6
+#define R21  40  // H
+#define R22  41  // J
+#define R23  42  // K
+#define R24  43  // L
+#define R25  44  // ;
+#define R26  45  // '
+
+// Row 3: bottom alpha
+#define L30  46  // LShift
+#define L31  47  // Z
+#define L32  48  // X
+#define L33  49  // C
+#define L34  50  // V
+#define L35  51  // B
+
+#define R30  54  // N
+#define R31  55  // M
+#define R32  56  // ,
+#define R33  57  // .
+#define R34  58  // /
+#define R35  59  // RShift
+
+// Row 4: bottom function row
+#define L40  60  // mo2
+#define L41  61  // `
+#define L42  62  // Caps
+#define L43  63  // Left
+#define L44  64  // Right
+
+#define R40  71  // Up
+#define R41  72  // Down
+#define R42  73  // [
+#define R43  74  // ]
+#define R44  75  // mo2
+
+// Thumb cluster (inner mod keys, top row)
+#define LH3  35  // LCtrl
+#define LH2  36  // LAlt
+#define RH2  37  // LGui
+#define RH3  38  // RCtrl
+
+// Thumb cluster (tall keys)
+#define LH1  65  // Bksp
+#define LH0  66  // Del
+#define RH0  69  // Enter
+#define RH1  70  // Space
+
+// Thumb cluster (nav keys)
+#define LHN1 52  // Home
+#define LHN0 67  // End
+#define RHN0 53  // PgUp
+#define RHN1 68  // PgDn
+
+// Group aliases (for HRM hold-trigger-key-positions)
+#define KEYS_L L00 L01 L02 L03 L04 L05 L10 L11 L12 L13 L14 L15 L20 L21 L22 L23 L24 L25 L30 L31 L32 L33 L34 L35 L40 L41 L42 L43 L44
+#define KEYS_R R00 R01 R02 R03 R04 R05 R10 R11 R12 R13 R14 R15 R20 R21 R22 R23 R24 R25 R30 R31 R32 R33 R34 R35 R40 R41 R42 R43 R44
+#define THUMBS LH0 LH1 LH2 LH3 RH0 RH1 RH2 RH3
